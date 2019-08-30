78743b451eb99829fe814394c3f31e6c6cf95661
Drews <kdrews@tech-res.com>
2019-08-30 11:56:27 -0400

    Sarf Start

---
+++ src/pages/DMID/SARF/userRequest.vue
@@ -0,0 +1,36 @@
+<template>
+</template>
+<script>
+export default {
+    components: {
+
+  },
+
+  data: function () {
+    return {
+
+    }
+  },
+  computed: {
+    stateComponent: {
+      get: function () {
+        return this.$root.store.state.activeComponent
+      },
+      set: function (value) {
+        this.$root.store.commit('isActiveComponent', value)
+      }
+    }
+  },
+  /// ////
+  mounted: function () {
+    this.stateActiveComponent('')
+  },
+  methods: {
+    stateActiveComponent (value) {
+      return this.stateComponent = value
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
