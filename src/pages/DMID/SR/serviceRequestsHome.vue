04ce509fe32c65269e1e2a53fb881943e3473351
Drews <kdrews@tech-res.com>
2019-09-05 09:25:01 -0400

    File content commit

---
+++ src/pages/DMID/SR/serviceRequestsHome.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    Service Request ServiceRequestsHome
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SR',
+  components: {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  /// ////
+  computed: {
+    stateTitle: {
+      get: function () {
+        return this.$root.store.state.siteTitle
+      },
+      set: function (value) {
+        this.$root.store.commit('isSiteTitle', value)
+      }
+    }
+  },
+  /// ////
+  mounted: function () {
+    this.$nextTick(function () {
+      this.stateActiveTitle('Service Requests')
+    })
+  },
+  /// ////
+  methods: {
+    stateActiveTitle (value) {
+      return this.stateTitle = value
+    },
+    showModal () {
+      this.isModalVisible = true
+    },
+    closeModal () {
+      this.isModalVisible = false
+    }
+
+  }
+}
+
+</script>
+<style>
+
+</style>
